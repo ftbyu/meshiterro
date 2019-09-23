d0690b8e66fe2022c40bd0a4fc0f0062fe2b9ce2
ftbyu <ftb.y.u.kashima.07@icloud.com>
2019-09-23 01:03:29 +0000

    Add PostImages

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a30cd9185259fe2942fa955255407c5c7d845a9bd97fd86d7efdc22b5baca19c9121f58298ed108bea994b65b236af00101b95a222546f030bd84e20e53402ad'
